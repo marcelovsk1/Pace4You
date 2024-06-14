c35d37392f4b32fa2227261ebe2ae23d0987fceb
Marcelo Amaral Alves <marceloaa@live.com>
2024-06-14 16:36:16 -0400

    working on

---
+++ Pace4You/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pace4You
+//
+//  Created by Marcelo Amaral Alves on 2024-06-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pace4You/Pace4YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pace4YouApp.swift
+//  Pace4You
+//
+//  Created by Marcelo Amaral Alves on 2024-06-14.
+//
+
+import SwiftUI
+
+@main
+struct Pace4YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
